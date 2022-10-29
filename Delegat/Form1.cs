d6033e63d115c177aa2bb68f65db88fa1da8c6e0
Даниил Бабушкин <nyta_210897@mail.ru>
2022-10-29 09:25:59 +0500

    Добавьте файлы проекта.

---
+++ Delegat/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegat
+{
+    public partial class Form1 : Form
+    {
+        string message;
+        void PrintSimpleMessag(string message) => listBox1.Items.Add(message);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
